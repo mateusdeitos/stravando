87046f1794e50d734d8e00aacec456a1ce398dda
mateusdeitos <mate.deitos@hotmail.com>
2021-04-27 14:05:22 -0300

    Melhorias

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -19,46 +19,51 @@ export const PageWrapper: React.FC<StackProps> = ({ children, ...rest }) => {
 				align="center"
 				justify="start"
 				m="0 auto"
-				// pb={["15rem", "0"]}
 				{...rest}
 			>
 				{children}
-				<Flex w="100%" direction="column" alignItems="center">
-					<Divider />
-					<Flex alignItems="center" mb="2rem">
-						<Text fontSize="1rem">Desenvolvido por&nbsp;</Text>
-						<Popover trigger="click" placement="top" >
-							<PopoverTrigger>
-								<Heading fontSize="1rem" fontWeight="600" color="brand.500" cursor="pointer" >Mateus Deitos</Heading>
-							</PopoverTrigger>
-							<PopoverContent bg="gray.800" borderColor="gray.800">
-								<PopoverHeader borderColor="gray.800">
-									<Flex alignItems="center">
-										<Avatar mr="1rem" size="md" showBorder src="https://avatars.githubusercontent.com/u/8628316?v=4" />
-										<Heading fontSize="1rem">Mateus Deitos</Heading>
-									</Flex>
-								</PopoverHeader>
-								<PopoverArrow bg="gray.800" />
-								<PopoverCloseButton />
-								<PopoverBody>
-									<Flex alignItems="center">
-										<Icon as={FaLinkedin} boxSize={5} />
-										<ChakraLink ml=".5rem" data-gtm="contato-linkedin" href="https://www.linkedin.com/in/mateus-deitos/" cursor="pointer" isExternal fontWeight="600" color="blue.500" textDecoration="underline">Linkedin</ChakraLink>
-									</Flex>
-									<Flex alignItems="center">
-										<Icon as={FaGithub} boxSize={5} />
-										<ChakraLink ml=".5rem" data-gtm="contato-github" href="https://www.github.com/mateusdeitos/" cursor="pointer" isExternal fontWeight="600" color="blue.500" textDecoration="underline">Github</ChakraLink>
-									</Flex>
-									<Flex alignItems="center">
-										<Icon as={FaEnvelope} boxSize={5} />
-										<ChakraLink ml=".5rem" data-gtm="contato-envelope" href="mailto:mate.deitos@hotmail.com" cursor="pointer" isExternal fontWeight="600" color="blue.500" textDecoration="underline">mate.deitos@hotmail.com</ChakraLink>
-									</Flex>
-								</PopoverBody>
-							</PopoverContent>
-						</Popover>
-					</Flex>
-				</Flex>
+				<Footer />
 			</VStack >
 		</>
 	)
+}
+
+const Footer = () => {
+	return (
+		<Flex w="100%" direction="column" alignItems="center">
+			<Divider />
+			<Popover trigger="click" placement="top" >
+				<PopoverTrigger>
+					<Flex alignItems="center" mb="2rem" mt="0.5rem">
+						<Heading fontWeight="normal" fontSize="1rem">Desenvolvido por&nbsp;</Heading>
+						<Heading fontSize="1rem" fontWeight="600" color="brand.500" cursor="pointer" >Mateus Deitos</Heading>
+					</Flex>
+				</PopoverTrigger>
+				<PopoverContent bg="gray.800" borderColor="gray.800">
+					<PopoverHeader borderColor="gray.800">
+						<Flex alignItems="center">
+							<Avatar mr="1rem" size="md" showBorder src="https://avatars.githubusercontent.com/u/8628316?v=4" />
+							<Heading fontSize="1rem">Mateus Deitos</Heading>
+						</Flex>
+					</PopoverHeader>
+					<PopoverArrow bg="gray.800" />
+					<PopoverCloseButton />
+					<PopoverBody>
+						<Flex alignItems="center">
+							<Icon as={FaLinkedin} boxSize={5} />
+							<ChakraLink ml=".5rem" data-gtm="contato-linkedin" href="https://www.linkedin.com/in/mateus-deitos/" cursor="pointer" isExternal fontWeight="600" color="blue.500" textDecoration="underline">Linkedin</ChakraLink>
+						</Flex>
+						<Flex alignItems="center">
+							<Icon as={FaGithub} boxSize={5} />
+							<ChakraLink ml=".5rem" data-gtm="contato-github" href="https://www.github.com/mateusdeitos/" cursor="pointer" isExternal fontWeight="600" color="blue.500" textDecoration="underline">Github</ChakraLink>
+						</Flex>
+						<Flex alignItems="center">
+							<Icon as={FaEnvelope} boxSize={5} />
+							<ChakraLink ml=".5rem" data-gtm="contato-envelope" href="mailto:mate.deitos@hotmail.com" cursor="pointer" isExternal fontWeight="600" color="blue.500" textDecoration="underline">mate.deitos@hotmail.com</ChakraLink>
+						</Flex>
+					</PopoverBody>
+				</PopoverContent>
+			</Popover>
+		</Flex>
+	);
 }
