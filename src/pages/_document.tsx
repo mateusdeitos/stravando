ee98da4139a05db857483551e78cf84f6b1012d6
mateusdeitos <mate.deitos@hotmail.com>
2021-04-28 08:12:41 -0300

    ajuste icones pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,27 +6,23 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<meta name='application-name' content='Stravando App' />
-					<meta name='apple-mobile-web-app-capable' content='yes' />
-					<meta name='apple-mobile-web-app-status-bar-style' content='default' />
-					<meta name='apple-mobile-web-app-title' content='Stravando App' />
-					<meta name='description' content='See your stats in Strava in a nice way' />
-					<meta name='format-detection' content='telephone=no' />
-					<meta name='mobile-web-app-capable' content='yes' />
-					<meta name='msapplication-TileColor' content='#fc5200' />
-					<meta name='msapplication-tap-highlight' content='no' />
-					<meta name='theme-color' content='#181B23' />
-
-					<link rel='apple-touch-icon' href='/favicon.png' />
-					<link rel='apple-touch-icon' sizes='152x152' href='/favicon.png' />
-					<link rel='apple-touch-icon' sizes='180x180' href='/favicon.png' />
-					<link rel='apple-touch-icon' sizes='167x167' href='/favicon.png' />
-
-					<link rel='icon' type='image/png' sizes='32x32' href='/firefox/firefox-general-32-32.png' />
-					<link rel='icon' type='image/png' sizes='16x16' href='/firefox/firefox-general-16-16.png' />
-					<link rel='manifest' href='/manifest.json' />
-					<link rel='mask-icon' href='/favicon.png' color='#fc5200' />
-					<link rel='shortcut icon' href='/favicon.ico' />
+					<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+					<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+					<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+					<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+					<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+					<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+					<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+					<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+					<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+					<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+					<link rel="manifest" href="/manifest.json" />
+					<meta name="msapplication-TileColor" content="#fc5200" />
+					<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
 
 					<meta name='twitter:card' content='summary' />
 					<meta name='twitter:url' content='https://stravando.vercel.app' />
@@ -40,16 +36,15 @@ export default class MyDocument extends Document {
 					<meta property='og:site_name' content='Stravando App' />
 					<meta property='og:url' content='https://stravando.vercel.app' />
 					<meta property='og:image' content='https://stravando.vercel.app/favicon.png' />
-					<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
 					<script dangerouslySetInnerHTML={{ __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N96LKJV');" }}></script>
 				</Head>
-				<body>
-					<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N96LKJV" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
-					<Main />
-					<NextScript />
-				</body>
+					<body>
+						<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N96LKJV" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+						<Main />
+						<NextScript />
+					</body>
 			</Html>
 		)
 	}
